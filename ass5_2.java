d661e892568c592682fe81367052b2eeeb7be704
Patil Dishank <40629684+BytesGod@users.noreply.github.com>
2019-09-25 13:43:14 +0530

    Add files via upload

---
+++ ass5_2.java
@@ -0,0 +1,13 @@
+import nameSorting.sorting;
+
+public class ass5_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        nameSorting.sorting d = new nameSorting.sorting();
+        d.sorts();  
+    }
+    
+}
---
+++ sorting.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nameSorting;
+import studentName.insertStudent;
+
+/**
+ *
+ * @author utkar
+ */
+import java.util.Scanner;
+
+public class sorting {
+
+   public void sorts(){
+       insertStudent d = new insertStudent();
+       d.insert();
+    }
+    
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
